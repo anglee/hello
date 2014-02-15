4a15618d42ce5b5fdacef56b8cd32a9f2414a2a3
anglee <ang.lee@gmail.com>
2014-02-15 16:36:47 -0500

    Use 'browser' instead of protractor.getInstance()

--- test/e2e/index/index.spec.js
+++ test/e2e/index/index.spec.js
@@ -1,9 +1,8 @@
 describe("hello-protractor", function () {
-    var ptor = protractor.getInstance();
     describe("index", function () {
         it("should display the correct title", function () {
-            ptor.get('app/#');
-            expect(ptor.getTitle()).toBe('hello');
+            browser.get('app/#');
+            expect(browser.getTitle()).toBe('hello');
         });
     });
 });
