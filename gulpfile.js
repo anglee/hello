50a406361f1c7872dd3268e1b5f8945a95bae462
anglee <ang.lee@gmail.com>
2014-02-26 05:22:44 -0500

    Introduced gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
 gulp.task('default', function(){
     // place code for your default task here
     console.log("Hello, gulp!");
+});
+
+gulp.task('hello', function () {
+    console.log("Hello");
+});
+
+gulp.task('script', function () {
+    gulp.src('./src/*.js')
+        .pipe(concat("all.js"))
+        .pipe(gulp.dest("./dist/"));
 });
