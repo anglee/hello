d66bfdb5630b69e6446c0db1fc61bf81a00b3e25
anglee <ang.lee@gmail.com>
2014-03-04 04:33:45 -0500

    Minor clean up

--- src/main/java/org/anglee/hello/module/HProvider.java
+++ src/main/java/org/anglee/hello/module/HProvider.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.anglee.hello.module;
 
 import com.google.inject.Inject;
@@ -11,14 +5,8 @@ import com.google.inject.Provider;
 
 /**
  *
- * @author alee
  */
 public class HProvider implements Provider<Hi> {
-
-  @Inject
-  HProvider() {
-
-  }
 
   @Override
   public Hi get() {
