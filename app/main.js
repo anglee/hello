3adae8ae7845e0ec4a9ed3e2ed358f7dc1b4df60
anglee <ang.lee@gmail.com>
2014-04-04 16:00:45 -0400

    Hello ui-router

--- app/main.js
+++ app/main.js
@@ -1,7 +1,39 @@
 !(function() {
   "use strict";
-  var myApp = angular.module("myApp", []);
-  myApp.controller("myCtrl", function($scope) {
-    $scope.foo = "bar";
+  var myApp = angular.module("myApp", ['ui.router']);
+  myApp.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'templates/home.html'
+        })
+        .state('list', {
+          url: '/list',
+          templateUrl: 'templates/list.html',
+          controller: 'ListCtrl'
+        })
+        .state('list.item', {
+          url: '/:itemName',
+          templateUrl: 'templates/list.item.html',
+          controller: function ($scope, $stateParams) {
+            $scope.name = $stateParams.itemName;
+          }
+        });
+  });
+  myApp.controller("HomeCtrl", function($scope) {
+    $scope.title = "HOME";
+  });
+  myApp.controller("ListCtrl", function($scope) {
+    $scope.items = [
+      {
+        name: "A"
+      },
+      {
+        name: "B"
+      },
+      {
+        name: "C"
+      }
+    ];
   });
 })();
