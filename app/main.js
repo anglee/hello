a5771edc8d8b9a91080776b1d7c5ae7f60aebec7
anglee <ang.lee@gmail.com>
2014-04-04 16:49:18 -0400

    Use Restangular instead of

--- app/main.js
+++ app/main.js
@@ -1,36 +1,14 @@
 !(function() {
   "use strict";
-  var myApp = angular.module("myApp", []);
-  myApp.factory("mySvc", function ($q, $http) {
-    return {
-      getGreeting: function (name) {
-        var d = $q.defer();
-        $http.get("hello/" + name).success(function (data) {
-          d.resolve(data.greeting);
-        });
-        return d.promise;
-      },
-      getTime: function () {
-        var d = $q.defer();
-        $http.get("time").success(function (data) {
-          d.resolve(data.time);
-        });
-        return d.promise;
-      }
-    }
-  });
+  var myApp = angular.module("myApp", ['restangular']);
 
-  myApp.controller("myCtrl", function ($scope, mySvc) {
-    var name = "Ang Lee"
-
-    // no more automatic promise unwrapping :-( and one of the reason was
-    // "adds too much magic"
-    // https://github.com/angular/angular.js/commit/5dc35b527b3c99f6544b8cb52e93c6510d3ac577
-    mySvc.getGreeting(name).then(function (greeting) {
-      $scope.greeting = greeting;
-    })
-    mySvc.getTime().then(function (time) {
-      $scope.time = time;
+  myApp.controller("myCtrl", function ($scope, Restangular) {
+    var name = 'Ang Lee'
+    Restangular.one('hello', name).get().then(function(data) {
+      $scope.greeting = data.greeting;
+    });
+    Restangular.one('time').get().then(function(data) {
+      $scope.time = data.time;
     });
   });
 })();
