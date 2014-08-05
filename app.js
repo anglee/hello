47aab7e7a7974f2aae2b5b3883c8cb7a76dffcc7
anglee <ang.lee@gmail.com>
2014-08-04 22:27:47 -0400

    The 'let' keyword

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-var a = 1 + 1;
-console.log(a);
+let message = "hi";
+
+{
+  let message = "bye";
+}
+
+console.log(message);
+
+// Note:
+// One needs
+// traceur --block-binding true
+// or
+// traceur --experimental
+// see https://github.com/google/traceur-compiler/wiki/LanguageFeatures#language-features
