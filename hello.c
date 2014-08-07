0e1a597aeedd84ab5f5515abe75346243a87cfa0
anglee <ang.lee@gmail.com>
2014-08-07 13:49:22 -0400

    After 'make'

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Defines printf etc.
+
+int main()
+{
+  // Write a string of text to the console:
+  printf( "Masters of the void." );
+	
+  return 0; // Tell OS everything is OK.
+}
+
