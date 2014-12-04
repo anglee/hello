ec02b8ad07b76c3891128cdafc956372d0142834
anglee <ang.lee@gmail.com>
2014-12-04 00:11:31 -0500

    Add example of importing library in Worker

--- worker.js
+++ worker.js
@@ -1,7 +1,12 @@
+importScripts("bower_components/lodash/dist/lodash.js")
 postMessage("I\'m working before postMessage(\'ali\').");
 
 onmessage = function (oEvent) {
   postMessage("Hi " + oEvent.data);
 };
 
+var a = 0;
+setInterval(function() {
+  console.log(_.range(3));
+}, 2000);
 console.log("test");
